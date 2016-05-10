85c1356657c1fd975acb88d5ee8792f24fd63b48
Chris Parry <wesbelden@gmail.com>
2016-05-10 23:44:05 +0100

    Created BattleTank project in UE4, saved a level and set the project settings to point to said level.

---
+++ BattleTank_Project/Source/BattleTank_Project/BattleTank_ProjectGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank_Project.h"
+#include "BattleTank_ProjectGameMode.h"
+
+
+
+
---
+++ BattleTank_Project/Source/BattleTank_Project/BattleTank_ProjectGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BattleTank_ProjectGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_PROJECT_API ABattleTank_ProjectGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
